760b0751f73e9f6361b14ef5de180dd3e7e73509
ranapelona <aldomacias20@gmail.com>
2026-03-17 00:10:39 -0700

    Bug fixes

--- adventure_game.py
+++ adventure_game.py
@@ -3,183 +3,197 @@ health = 100
 hunger = 50
 miles_traveled = 0
 bolas = 0
-scraps= 0
+scraps = 0
 
 min_dmg = 5
 max_dmg = 15
 weapon_lvl = 1
 
-while True:
-    
-    h_bar = "█" * (health//10)
-    hungr_bar = "█" * (hunger//10)
-    print(f"\n" + "="*40)
-    print(f"HEALTH: [{h_bar:<10}] {health}%")
-    print(f"HUNGER: [{hungr_bar:<10}] {hunger}%")
-    print(f"bolas: {bolas}  SCRAPS: {scraps} MILES: {miles_traveled}")
-    print(f"Weapon LVL: {weapon_lvl} (Dmg: {min_dmg}-{max_dmg})")
-    print("="*40)
-    
-    selection = input("You are in a cursed desert. Will you [Rest], [Explore], [Eat], [Scavenge], [Dungeon], [Upgrade], or [Quit]? ").lower()
+if __name__ == "__main__":
+    while True:
 
-    if selection == "rest":
-        health = min(100, health +10)
-        hunger = hunger + 2
-        print("You had a good nap gained 10 health and got two points hungrier.")
+        h_bar = "█" * (health//10)
+        hungr_bar = "█" * (hunger//10)
+        print(f"\n" + "="*40)
+        print(f"HEALTH: [{h_bar:<10}] {health}%")
+        print(f"HUNGER: [{hungr_bar:<10}] {hunger}%")
+        print(f"bolas: {bolas}  SCRAPS: {scraps} MILES: {miles_traveled}")
+        print(f"Weapon LVL: {weapon_lvl} (Dmg: {min_dmg}-{max_dmg})")
+        print("="*40)
 
+        selection = input(
+            "You are in a cursed desert. Will you [Rest], [Explore], [Eat], [Scavenge], [Dungeon], [Upgrade], or [Quit]? ").lower()
 
-    elif selection == "eat":
-        hunger = max(0,hunger - 8)
-        print("You ate a scrumchous burger, your hunger has decreased eight points.")
+        if selection == "rest":
+            health = min(100, health + 10)
+            hunger = hunger + 2
+            print("You had a good nap gained 10 health and got two points hungrier.")
 
+        elif selection == "eat":
+            hunger = max(0, hunger - 8)
+            print("You ate a scrumchous burger, your hunger has decreased eight points.")
 
-    elif selection == "dungeon":
-        print("\n--- ENTERING THE CURSED CAVE ---")
-        confirm = input("It's dangerous inside. Are you sure ? [Yes/No] ").lower()
-        if confirm == "yes":
-            health -= 5
-            hunger += 10
-            
-            event = random.randint(1, 2)
-            if event == 1:
-                print("\n💀 A SKELLY CAME OUT DANCING CUMBIA AND IS READY TO FIGHT !")
-                skelly_hp = 20
-                while skelly_hp > 0 and health > 0:
-                    print(f">> YOUR HP: {health}% SKELLY HP: {skelly_hp}")
-                    action = input("Do you [Slash] or [Dodge]? ").lower()
+        elif selection == "dungeon":
+            print("\n--- ENTERING THE CURSED CAVE ---")
+            confirm = input(
+                "It's dangerous inside. Are you sure ? [Yes/No] ").lower()
+            if confirm == "yes":
+                health -= 5
+                hunger += 10
 
-                    if action == "slash":
-                        damage = random.randint(min_dmg, max_dmg)
-                        skelly_hp -= damage
-                        skelly_hit = random.randint(3, 7)
-                        health -= skelly_hit
-                        print(f"You hit the skelly for {damage}! Skelly hit you for {skelly_hit}!")
-                    elif action == "dodge":
-                        health += 2
-                        print("You danced around his rusty sword and gained 2 HP.")
+                event = random.randint(1, 2)
+                if event == 1:
+                    print(
+                        "\n💀 A SKELLY CAME OUT DANCING CUMBIA AND IS READY TO FIGHT !")
+                    skelly_hp = 20
+                    while skelly_hp > 0 and health > 0:
+                        print(f">> YOUR HP: {health}% SKELLY HP: {skelly_hp}")
+                        action = input("Do you [Slash] or [Dodge]? ").lower()
+
+                        if action == "slash":
+                            damage = random.randint(min_dmg, max_dmg)
+                            skelly_hp -= damage
+                            skelly_hit = random.randint(3, 7)
+                            health -= skelly_hit
+                            print(
+                                f"You hit the skelly for {damage}! Skelly hit you for {skelly_hit}!")
+                        elif action == "dodge":
+                            health += 2
+                            print(
+                                "You danced around his rusty sword and gained 2 HP.")
+                        else:
+                            print(
+                                " You were dazed and confused so the skelly hit you ! -8 HP.")
+                            health -= 8
+                    if health > 0:
+                        loot = random.randint(10, 30)
+                        bolas += loot
+                        print(
+                            f"The skelly has been felled. You found {loot} bolas in his baggie.")
+
+                else:
+                    roll = random.randint(1, 4)
+                    if roll == 1:
+                        found_scraps = random.randint(2, 5)
+                        scraps += found_scraps
+                        print(
+                            f"You found a hidden armory ! + {found_scraps} scraps.")
+                    elif roll == 2:
+                        found_bolas = random.randint(40, 80)
+                        bolas += found_bolas
+                        print(
+                            f"You found a golden sarcophagus ! +{found_bolas} bolas.")
+                    elif roll == 3:
+                        damage = 20
+                        health -= damage
+                        print(f"You triggered a dart trap ! -{damage} Health.")
+                        print(f"Your health is now {health}%.")
                     else:
-                        print(" You were dazed and confused so the skelly hit you ! -8 HP.")
-                        health -= 8
-                if health > 0:
-                    loot = random.randint(10, 30)
-                    bolas += loot
-                    print(f"The skelly has been felled. You found {loot} in his baggie.")
-            
+                        print("The cave was empty, but you found a cool rock.")
             else:
-                roll = random.randint(1, 4)
-                if roll == 1:
-                 found_scraps = random.randint(2, 5)
-                 scraps += found_scraps
-                 print(f"You found a hidden armory ! + {found_scraps} scraps.")
-                elif roll == 2:
-                 found_bolas = random.randint(40, 80)
-                 bolas += found_bolas
-                 print(f"You found a golden sarcophagus ! +{found_bolas} bolas.")
-                elif roll == 3:
-                 damage = 20
-                 health -= damage
-                 print(f"You triggered a dart trap ! -{damage} Health.")
-                 print(f"Your health is now {health}%.")
-                else:
-                 print("The cave was empty, but you found a cool rock.")
-        else:
-         print("You changed your mind")
-    
-    
-    elif selection == "scavenge":
-        roll = random.randint(1, 3)
-        if roll == 1:
-            found_bolas = random.randint(5, 15)
-            bolas = bolas + found_bolas
-            print(f"You found a lost pouch ! +{found_bolas} bolas")
-        elif roll == 2:
-            scraps = scraps + 1
-            print("You found some old rusty metal scraps!")
-        else:
-            health = health - 10
-            print("A scorpion stung you !  -10 Health.")
-    
-    elif selection == "upgrade":
-        if bolas >= 20 and scraps >= 2:
-            bolas = bolas - 20
-            scraps = scraps - 2
-            weapon_lvl = weapon_lvl +1
-            min_dmg = min_dmg + 3
-            max_dmg = max_dmg + 5
-            print(f"UPGRADE SUCCESS! Weapon is now level {weapon_lvl}!")
-        else: print(f"Not enough resources ! Need 20 bolas & 2 scraps. You have: {bolas}B, {scraps}S)")
-    
-    elif selection == "quit":
-        print("Thanks for playing guy.")
-        break
-    
-    elif selection == "explore":
-          miles_traveled = miles_traveled + 10
-          health = health - 2
-          hunger = hunger + 5
-          print(f"You run forward. Total distance: {miles_traveled} miles.")
+                print("You changed your mind")
 
-          if miles_traveled == 10:
-             print("\n!!! JORGE EL TERRIBLE HAS RISEN FROM UNDER THE SAND !!!")
-             jorge_hp = 30
-             while jorge_hp > 0 and health > 0:
-              h_bar = "█" * min(10, health // 10)
-              
-              print(f"\n>> YOUR HP: [{h_bar:<10}] {health}%  JORGE HP: {jorge_hp}")
+        elif selection == "scavenge":
+            roll = random.randint(1, 3)
+            if roll == 1:
+                found_bolas = random.randint(5, 15)
+                bolas = bolas + found_bolas
+                print(f"You found a lost pouch ! +{found_bolas} bolas")
+            elif roll == 2:
+                scraps = scraps + 1
+                print("You found some old rusty metal scraps!")
+            else:
+                health = health - 10
+                print("A scorpion stung you !  -10 Health.")
 
-              fight = input("Do you [Slash] or [Dodge]").lower()
-                
-              if fight == "slash":
-                damage = random.randint(min_dmg, max_dmg)
-                jorge_hp -= damage
-                boss_hit = random.randint(5, 10)
-                health -= boss_hit
-                print(f"You hit Georgie for {damage}! He hit you back for {boss_hit}!")
-              elif fight == "dodge":
-                  health = health + 2
-                  print("You dodged Jorge's fangs! You took 0 damage and gained 2 points.")
-              else:
-                    print("You stood confused! Jorge gets a free hit.")
-                    health -= 10
-             if health > 0:
-                print("You have defeated Jorge El Terrible! Continue your journey.")
+        elif selection == "upgrade":
+            if bolas >= 20 and scraps >= 2:
+                bolas = bolas - 20
+                scraps = scraps - 2
+                weapon_lvl = weapon_lvl + 1
+                min_dmg = min_dmg + 3
+                max_dmg = max_dmg + 5
+                print(f"UPGRADE SUCCESS! Weapon is now level {weapon_lvl}!")
+            else:
+                print(
+                    f"Not enough resources ! Need 20 bolas & 2 scraps. You have: {bolas}B, {scraps}S")
 
-        
-        
-          elif miles_traveled == 20:
-            print("\n!!! YOU HAVE REACHED THE PYRAMID. THE MUMMY IS HERE !!!")
-            print("He holds Rosalina inside his sarcophagus")
-            mummy_hp = 60
-            while mummy_hp > 0 and health > 0:
-                h_bar ="█" * min(10, health // 10)
-                
-                print(f"\n>>  YOUR HP: [{h_bar:<10}] {health}% MUMMY HP: {mummy_hp}")
-                
-                attack = input("The mummy has cursed you ! [Attack] or [Dodge]? ").lower()
-                
-                if attack == "attack":
-                    damage = random.randint(min_dmg, max_dmg)
-                    mummy_hp -= damage
-                    boss_hit = random.randint(5, 10)
-                    health -= boss_hit
-                    print(f"You hit the Mummy for {damage} damage! He hit you back for {boss_hit}")
-                elif attack == "dodge":
-                    health = health + 2
-                    print("You have dodged the Mummy's hit ! You took 0 damage and gained 2 health points.")
-                else:
-                    print("The curse is wearing you out, you take damage!")
-                    health -= 15
-            if health > 0:
-                    print("The Mummy's soul explodes and lets out a big roar ! The curse has been lifted !")
+        elif selection == "quit":
+            print("Thanks for playing guy.")
+            break
+
+        elif selection == "explore":
+            miles_traveled = miles_traveled + 10
+            health = health - 2
+            hunger = hunger + 5
+            print(f"You run forward. Total distance: {miles_traveled} miles.")
+
+            if miles_traveled == 10:
+                print("\n!!! JORGE EL TERRIBLE HAS RISEN FROM UNDER THE SAND !!!")
+                jorge_hp = 30
+                while jorge_hp > 0 and health > 0:
+                    h_bar = "█" * min(10, health // 10)
+                    print(
+                        f"\n>> YOUR HP: [{h_bar:<10}] {health}%  JORGE HP: {jorge_hp}")
+
+                    fight = input("Do you [Slash] or [Dodge]").lower()
+
+                    if fight == "slash":
+                        damage = random.randint(min_dmg, max_dmg)
+                        jorge_hp -= damage
+                        boss_hit = random.randint(5, 10)
+                        health -= boss_hit
+                        print(
+                            f"You hit Georgie for {damage}! He hit you back for {boss_hit}!")
+                    elif fight == "dodge":
+                        health = health + 2
+                        print(
+                            "You dodged Jorge's fangs! You took 0 damage and gained 2 points.")
+                    else:
+                        print("You stood confused! Jorge gets a free hit.")
+                        health -= 10
+                if health > 0:
+                    print("You have defeated Jorge El Terrible! Continue your journey.")
+
+            elif miles_traveled == 20:
+                print("\n!!! YOU HAVE REACHED THE PYRAMID. THE MUMMY IS HERE !!!")
+                print("He holds Rosalina inside his sarcophagus")
+                mummy_hp = 60
+                while mummy_hp > 0 and health > 0:
+                    h_bar = "█" * min(10, health // 10)
+
+                    print(
+                        f"\n>>  YOUR HP: [{h_bar:<10}] {health}% MUMMY HP: {mummy_hp}")
+
+                    attack = input(
+                        "The mummy has cursed you ! [Attack] or [Dodge]? ").lower()
+
+                    if attack == "attack":
+                        damage = random.randint(min_dmg, max_dmg)
+                        mummy_hp -= damage
+                        boss_hit = random.randint(5, 10)
+                        health -= boss_hit
+                        print(
+                            f"You hit the Mummy for {damage} damage! He hit you back for {boss_hit}")
+                    elif attack == "dodge":
+                        health = health + 2
+                        print(
+                            "You have dodged the Mummy's hit ! You took 0 damage and gained 2 health points.")
+                    else:
+                        print("The curse is wearing you out, you take damage!")
+                        health -= 15
+                if health > 0:
+                    print(
+                        "The Mummy's soul explodes and lets out a big roar ! The curse has been lifted !")
                     print("You rescue Rosalina. YOU WIN")
                     break
-    else:
-        print("I don't understand that action.") 
+        else:
+            print("I don't understand that action.")
 
-    if health <= 0:
-           print("You Died, Game over.")
-           break
-          
-    if hunger >= 100:
-           print("You have starved to death, Game over.")
-           break
+        if health <= 0:
+            print("You Died, Game over.")
+            break
+
+        if hunger >= 100:
+            print("You have starved to death, Game over.")
+            break
